711133d2e1c2d68677d269ea187b4396401d069b
honjow <honjow311@gmail.com>
2025-09-07 20:21:06 +0800

    update fstab

--- all-install.sh
+++ all-install.sh
@@ -144,7 +144,7 @@ Subsystem	sftp	/usr/lib/ssh/sftp-server
 " > /etc/ssh/sshd_config
 
 echo "
-LABEL=frzr_efi  /boot      vfat      defaults,rw,nofail		0	0
+LABEL=frzr_efi  /boot      vfat      defaults,rw,noatime,nodiratime,nofail		0	2
 LABEL=frzr_root /var       btrfs     defaults,subvol=var,rw,noatime,nodatacow,nofail		0	0
 LABEL=frzr_root /home      btrfs     defaults,subvol=home,rw,noatime,nodatacow,nofail		0	0
 LABEL=frzr_root /frzr_root btrfs     defaults,subvol=/,rw,noatime,nodatacow,x-initrd.mount	0	2
