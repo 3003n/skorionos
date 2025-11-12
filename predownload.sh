99a723fd13501c9e40a81c8f906334b3dffb12f7
honjow <honjow311@gmail.com>
2025-11-12 10:49:39 +0800

    chore: comment out MangoPeel plugin download in predownload.sh

--- predownload.sh
+++ predownload.sh
@@ -250,7 +250,7 @@ all_download() {
 	download_decky_plugin "chenx-dust/BetterKeyboard" $decky_plugin_path ".zip"
 
 	# honjow/MangoPeel
-	download_decky_plugin "honjow/MangoPeel" $decky_plugin_path ".zip"
+	# download_decky_plugin "honjow/MangoPeel" $decky_plugin_path ".zip"
 
 	# pre download css themes
 
